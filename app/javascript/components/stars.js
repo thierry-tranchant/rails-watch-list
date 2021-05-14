5681bba4912710f0b1325b92b2321601c53b3ba9
Thierry Tranchant <thierry.tcht@gmail.com>
2021-05-14 17:48:57 +0200

    Encapsulated logic in stars.js to prevent invisible crash on home page.

--- app/javascript/components/stars.js
+++ app/javascript/components/stars.js
@@ -1,50 +1,62 @@
-const star_icons = document.querySelector('fieldset').querySelectorAll('.form-check');
+let mouseOverStars = () => {
+  return;
+}
 
-const getActive = (star) => {
-  star.addEventListener('mouseover', () => {
-    if (!star.querySelector('i').classList.contains('clicked')) {
+if (document.querySelector('#show-list') !== null) {
+
+  const star_icons = document.querySelector('fieldset').querySelectorAll('.form-check');
+
+  const getActive = (star) => {
+    star.addEventListener('mouseover', () => {
+      if (!star.querySelector('i').classList.contains('clicked')) {
+        const stars_array = Array.from(star_icons);
+        stars_array.forEach((star_icon) => {
+          if (stars_array.indexOf(star_icon) <= stars_array.indexOf(star)) {
+            star_icon.querySelector('i').classList.add('active');
+          } else {
+            star_icon.querySelector('i').classList.remove('active');
+          }
+        })
+      }
+    })
+  };
+
+  const getUnactive = (star) => {
+    star.addEventListener('mouseout', () => {
+      if (!star.querySelector('i').classList.contains('clicked')) {
+        const stars_array = Array.from(star_icons);
+        stars_array.forEach((star_icon) => {
+          if (!star_icon.querySelector('i').classList.contains('clicked')) {
+            star_icon.querySelector('i').classList.remove('active');
+          }
+        })
+      }
+    })
+  }
+
+  const getClicked = (star) => {
+    star.addEventListener('click', () => {
       const stars_array = Array.from(star_icons);
       stars_array.forEach((star_icon) => {
         if (stars_array.indexOf(star_icon) <= stars_array.indexOf(star)) {
-          star_icon.querySelector('i').classList.add('active');
+          star_icon.querySelector('i').classList.add('active', 'clicked');
         } else {
-          star_icon.querySelector('i').classList.remove('active');
+          star_icon.querySelector('i').classList.remove('active', 'clicked');
         }
       })
-    }
-  })
-};
-
-const getUnactive = (star) => {
-  star.addEventListener('mouseout', () => {
-    if (!star.querySelector('i').classList.contains('clicked')) {
-      const stars_array = Array.from(star_icons);
-      stars_array.forEach((star_icon) => {
-        if (!star_icon.querySelector('i').classList.contains('clicked')) {
-        star_icon.querySelector('i').classList.remove('active');
-        }
-      })
-    }
-  })
-}
-
-const getClicked = (star) => {
-  star.addEventListener('click', () => {
-    const stars_array = Array.from(star_icons);
-    stars_array.forEach((star_icon) => {
-      if (stars_array.indexOf(star_icon) <= stars_array.indexOf(star)) {
-        star_icon.querySelector('i').classList.add('active', 'clicked');
-      } else {
-        star_icon.querySelector('i').classList.remove('active', 'clicked');
-      }
     })
-  })
+  }
+
+  mouseOverStars = () => {
+    star_icons.forEach(getActive);
+    star_icons.forEach(getUnactive);
+    star_icons.forEach(getClicked);
+  }
+
 }
 
-const mouseOverStars = () => {
-  star_icons.forEach(getActive);
-  star_icons.forEach(getUnactive);
-  star_icons.forEach(getClicked);
-}
+export { mouseOverStars };
 
-export { mouseOverStars } ;
+
+
+
